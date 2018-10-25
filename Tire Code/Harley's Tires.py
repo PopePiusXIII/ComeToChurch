dd8bd1bd5f1358eba42d0143943ef521e94abb6b
DESKTOP-0FTP7D0\harhi <harleyhicksgto@gmail.com>
2018-10-24 22:25:17 -0400

    unlimited tire comparisons update to GUI

--- Tire Code/Harley's Tires.py
+++ Tire Code/Harley's Tires.py
@@ -4,6 +4,7 @@ from __future__ import division
 # from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
 # from matplotlib.figure import Figure
 # import numpy as np
+from collections import OrderedDict
 import Tkinter as Ttk
 # import Tire_Fitting
 from tkFileDialog import askopenfilename
@@ -71,59 +72,90 @@ class StartPage(Ttk.Frame):
         self.entry_frame = Ttk.Frame()
         self.entry_frame.grid(row=1, column=0)
 
-        self.tire_name = Ttk.StringVar(parent)
-        self.data_type = Ttk.StringVar(parent)
-        self.load = Ttk.StringVar(parent)
-        self.camber = Ttk.StringVar(parent)
-        self.pressure = Ttk.StringVar(parent)
-        self.speed = Ttk.StringVar(parent)
-        self.slip_sign = Ttk.StringVar(parent)
-        self.tire_dict = None
+        self.tire_name = [Ttk.StringVar()]
+        self.data_type = [Ttk.StringVar()]
+        self.load = [Ttk.StringVar()]
+        self.camber = [Ttk.StringVar()]
+        self.pressure = [Ttk.StringVar()]
+        self.speed = [Ttk.StringVar()]
+        self.slip_sign = [Ttk.StringVar()]
+        self.tire_dict = OrderedDict()
+        self.graph_bool = [Ttk.IntVar()]
         # corner.set('Choose Corner')
         # x_axis.set('x_axis')
         # y_axis.set('y_axis')
-        Ttk.Button(self.entry_frame, text='Update', command=lambda: self.grid_drop_downs()).grid(row=2, column=0)
-        Ttk.Button(self.entry_frame, text='Graph', command=lambda:
-                   Fy.raw_slip_force_plot(self.tire_dict["Slip Angle (Steady)"][self.speed.get()][self.pressure.get()][self.camber.get()]
-                                          [self.slip_sign.get()][self.load.get()],
-                                          self.tire_dict["Fy (Steady)"][self.speed.get()][self.pressure.get()][self.camber.get()]
-                                          [self.slip_sign.get()][self.load.get()], None, None, None, None
-                                          )).grid(row=2, column=1)
+        self.update = Ttk.Button(self.entry_frame, text='Update', command=lambda: self.grid_drop_downs())
+        self.update.grid(row=2, column=0)
+        self.graph = Ttk.Button(self.entry_frame, text='Graph', command=lambda: self.graph_func())
+        self.graph.grid(row=2, column=1)
+
+    def graph_func(self):
+        colors = ['r', 'b', 'g']
+        x_data=[]
+        y_data=[]
+        for i in range(0, len(self.tire_name)):
+            if self.graph_bool[i].get() == 1:
+                x_data.append(self.tire_dict[self.tire_name[i].get()]["Slip Angle (Steady)"]
+                               [self.speed[i].get()][self.pressure[i].get()][self.camber[i].get()]
+                               [self.slip_sign[i].get()][self.load[i].get()])
+                y_data.append(self.tire_dict[self.tire_name[i].get()]["Fy (Steady)"][self.speed[i].get()]
+                               [self.pressure[i].get()][self.camber[i].get()]
+                               [self.slip_sign[i].get()][self.load[i].get()])
+        Fy.raw_slip_force_plot(x_data, y_data, colors, None, None, None)
+        return
 
     def grid_drop_downs(self):
-        self.tire_dict = Fy.fy_data_collector(HarleyTires.file_paths[0])
-        data_name_options = self.tire_dict.keys()
-        tire_name_options = [HarleyTires.file_paths[0]]
-        speed_options = self.tire_dict['Speed (Test)']
-        slip_sign_options = self.tire_dict['Slip Sign (Test)']
-        pressure_options = self.tire_dict['Pressure (Test)']
-        camber_options = self.tire_dict['Camber (Test)']
-        load_options = self.tire_dict['Load (Test)']
+        self.update.destroy()
+        self.graph.destroy()
 
-        Ttk.Label(self.entry_frame, text='Name').grid(row=0, column=0)
-        tire_name_menu = Ttk.OptionMenu(self.entry_frame, self.tire_name, *tire_name_options)
+        self.tire_name.append(Ttk.StringVar())
+        self.data_type.append(Ttk.StringVar())
+        self.load.append(Ttk.StringVar())
+        self.camber.append(Ttk.StringVar())
+        self.pressure.append(Ttk.StringVar())
+        self.speed.append(Ttk.StringVar())
+        self.slip_sign.append(Ttk.StringVar())
+        self.graph_bool.append(Ttk.IntVar())
+        tire_number = len(HarleyTires.file_paths)
+        self.tire_dict[HarleyTires.file_paths[tire_number-1]] = Fy.fy_data_collector(HarleyTires.file_paths[tire_number-1])
+        tire_name_options = HarleyTires.file_paths
+        speed_options = self.tire_dict[HarleyTires.file_paths[tire_number-1]]['Speed (Test)']
+        slip_sign_options = self.tire_dict[HarleyTires.file_paths[tire_number-1]]['Slip Sign (Test)']
+        pressure_options = self.tire_dict[HarleyTires.file_paths[tire_number-1]]['Pressure (Test)']
+        camber_options = self.tire_dict[HarleyTires.file_paths[tire_number-1]]['Camber (Test)']
+        load_options = self.tire_dict[HarleyTires.file_paths[tire_number-1]]['Load (Test)']
+        Ttk.Label(self.entry_frame, text='Name').grid(row=tire_number, column=0)
+        tire_name_menu = Ttk.OptionMenu(self.entry_frame, self.tire_name[tire_number-1], *tire_name_options)
         tire_name_menu.config(width=13)
-        tire_name_menu.grid(row=1, column=0)
-        Ttk.Label(self.entry_frame, text='Speed').grid(row=0, column=1)
-        speed_menu = Ttk.OptionMenu(self.entry_frame, self.speed, *speed_options)
+        tire_name_menu.grid(row=tire_number, column=0)
+        Ttk.Label(self.entry_frame, text='Speed').grid(row=tire_number, column=1)
+        speed_menu = Ttk.OptionMenu(self.entry_frame, self.speed[tire_number-1], *speed_options)
         speed_menu.config(width=4)
-        speed_menu.grid(row=1, column=1)
-        Ttk.Label(self.entry_frame, text='Pressure').grid(row=0, column=2)
-        pressure_menu = Ttk.OptionMenu(self.entry_frame, self.pressure, *pressure_options)
+        speed_menu.grid(row=tire_number, column=1)
+        Ttk.Label(self.entry_frame, text='Pressure').grid(row=tire_number, column=2)
+        pressure_menu = Ttk.OptionMenu(self.entry_frame, self.pressure[tire_number-1], *pressure_options)
         pressure_menu.config(width=12)
-        pressure_menu.grid(row=1, column=2)
-        Ttk.Label(self.entry_frame, text='Camber').grid(row=0, column=3)
-        camber_menu = Ttk.OptionMenu(self.entry_frame, self.camber, *camber_options)
+        pressure_menu.grid(row=tire_number, column=2)
+        Ttk.Label(self.entry_frame, text='Camber').grid(row=tire_number, column=3)
+        camber_menu = Ttk.OptionMenu(self.entry_frame, self.camber[tire_number-1], *camber_options)
         camber_menu.config(width=6)
-        camber_menu.grid(row=1, column=3)
-        Ttk.Label(self.entry_frame, text='Slip Sign').grid(row=0, column=4)
-        slip_sign_menu = Ttk.OptionMenu(self.entry_frame, self.slip_sign, *slip_sign_options)
+        camber_menu.grid(row=tire_number, column=3)
+        Ttk.Label(self.entry_frame, text='Slip Sign').grid(row=tire_number, column=4)
+        slip_sign_menu = Ttk.OptionMenu(self.entry_frame, self.slip_sign[tire_number-1], *slip_sign_options)
         slip_sign_menu.config(width=9)
-        slip_sign_menu.grid(row=1, column=4)
-        Ttk.Label(self.entry_frame, text='Load').grid(row=0, column=5)
-        load_menu = Ttk.OptionMenu(self.entry_frame, self.load, *load_options)
+        slip_sign_menu.grid(row=tire_number, column=4)
+        Ttk.Label(self.entry_frame, text='Load').grid(row=tire_number, column=5)
+        load_menu = Ttk.OptionMenu(self.entry_frame, self.load[tire_number-1], *load_options)
         load_menu.config(width=4)
-        load_menu.grid(row=1, column=5)
+        load_menu.grid(row=tire_number, column=5)
+        Ttk.Label(self.entry_frame, text='Graph').grid(row=tire_number, column=6)
+        graph_bool = Ttk.Checkbutton(self.entry_frame, variable=self.graph_bool[tire_number-1])
+        graph_bool.grid(row=tire_number, column=6)
+        self.update = Ttk.Button(self.entry_frame, text='Update', command=lambda: self.grid_drop_downs())
+        self.update.grid(row=tire_number+1, column=0)
+        self.graph = Ttk.Button(self.entry_frame, text='Graph', command=lambda: self.graph_func())
+        self.graph.grid(row=tire_number+1, column=1)
+
         return
 
 
